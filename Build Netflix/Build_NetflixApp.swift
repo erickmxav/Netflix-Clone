c5fb097f9d36fd50766977875ad9b09ca6b8f83b
erickmxav <erickmxav@gmail.com>
2022-10-05 22:15:55 -0300

    start project

---
+++ Build Netflix/Build_NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build_NetflixApp.swift
+//  Build Netflix
+//
+//  Created by Vá de Táxi on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Build_NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
